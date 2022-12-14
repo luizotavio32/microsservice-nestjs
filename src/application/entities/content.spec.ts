01506bd4cb367220dc5fca4afb1b51bbd9b537a6
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-12-14 16:51:39 -0300

    test: contente greater than 5 lenght and smaller than 240

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,17 +1,17 @@
 import { Content } from './content';
 
 describe('Notification content', () => {
-  it('it should be able to create notification content', () => {
+  it('should be able to create notification content', () => {
     const content = new Content('Você recebeu uma solicitação de ammizade');
 
     expect(content).toBeTruthy();
   });
 
-  it('it should not be able to create notification content with less than 5 characters', () => {
+  it('should not be able to create notification content with less than 5 characters', () => {
     expect(() => new Content('test')).toThrow();
   });
 
-  it('it should not be able to create notification content with more than 240 characters', () => {
+  it('should not be able to create notification content with more than 240 characters', () => {
     expect(() => new Content('a'.repeat(241))).toThrow();
   });
 });
