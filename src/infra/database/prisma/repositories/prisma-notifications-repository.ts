8c2afe7c92ee08110041b03a9e7c914495d5b571
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-12-15 14:53:42 -0300

    feat: add findBy and save methods to notfc repos

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -7,6 +7,9 @@ import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper'
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prisma: PrismaService) {}
+  async findById(notificationId: string): Promise<Notification> {
+    throw new Error('Method not implemented.');
+  }
 
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
@@ -14,5 +17,9 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
     await this.prisma.notification.create({
       data: raw,
     });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -4,9 +4,17 @@ import { NotificationsRepository } from '@application/repositories/notification-
 export class InMemoryNotificationsRepository
   implements NotificationsRepository
 {
+  async findById(notificationId: string): Promise<Notification> {
+    throw new Error('Method not implemented.');
+  }
+
   public notifications: Notification[] = [];
 
   async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
