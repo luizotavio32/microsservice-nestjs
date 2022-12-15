3e4e9b0c32c9683e2953104b26d38d6f8f00d771
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-12-15 15:12:20 -0300

    test: cancel notification

---
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,39 @@
+import { Content } from '@application/entities/content';
+import { Notification } from '@application/entities/notification';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+describe('Cancel notification', () => {
+  it('should be able to cancel a notification', async () => {
+    const notificationRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationRepository);
+
+    const notification = new Notification({
+      content: new Content('This is a notification'),
+      category: 'social',
+      recipientId: 'this-is-a-recipient-id',
+    });
+
+    await notificationRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a non existent notification', async () => {
+    const notificationRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
