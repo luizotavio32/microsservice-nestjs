f8a3c570d02d6fd6c2ba31d61c5a85a1b6705159
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-12-15 15:53:52 -0300

    refactor: create notification using factory

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -1,5 +1,6 @@
 import { Content } from '@application/entities/content';
 import { Notification } from '@application/entities/notification';
+import { makeNotification } from '@test/factories/notification-factory';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CancelNotification } from './cancel-notification';
 import { NotificationNotFound } from './errors/notification-not-found';
@@ -9,11 +10,7 @@ describe('Cancel notification', () => {
     const notificationRepository = new InMemoryNotificationsRepository();
     const cancelNotification = new CancelNotification(notificationRepository);
 
-    const notification = new Notification({
-      content: new Content('This is a notification'),
-      category: 'social',
-      recipientId: 'this-is-a-recipient-id',
-    });
+    const notification = makeNotification();
 
     await notificationRepository.create(notification);
 
