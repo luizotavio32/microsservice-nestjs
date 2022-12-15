91cb92197154d3844dc83b5a4de30c572ee08a61
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-12-15 15:54:10 -0300

    feat: count notfc

---
+++ src/application/use-cases/count-recipient-notifications.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notification-repository';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+interface CountRecipientNotificationsRequest {
+  recipientId: string;
+}
+
+interface CountRecipientNotificationsResponse {
+  count: number;
+}
+
+@Injectable()
+export class CountRecipientNotifications {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(
+    request: CountRecipientNotificationsRequest,
+  ): Promise<CountRecipientNotificationsResponse> {
+    const { recipientId } = request;
+
+    const count = await this.notificationRepository.countManyByRecipientId(
+      recipientId,
+    );
+
+    return { count };
+  }
+}
