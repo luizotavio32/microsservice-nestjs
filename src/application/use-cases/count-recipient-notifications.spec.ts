07316de597f42935cef6a20242389c573a1c11dd
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-12-15 15:54:15 -0300

    test: count notfc

---
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -0,0 +1,30 @@
+import { Content } from '@application/entities/content';
+import { Notification } from '@application/entities/notification';
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CountRecipientNotifications } from './count-recipient-notifications';
+
+describe('Count recipient notification', () => {
+  it('should be able to count recipient notifications', async () => {
+    const notificationRepository = new InMemoryNotificationsRepository();
+    const countRecipientNotifications = new CountRecipientNotifications(
+      notificationRepository,
+    );
+
+    await notificationRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+    await notificationRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+    await notificationRepository.create(
+      makeNotification({ recipientId: 'recipient-2' }),
+    );
+
+    const { count } = await countRecipientNotifications.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(count).toEqual(2);
+  });
+});
