f08361296252b39e2e795b264e2a2e6e7957fe4e
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-12-21 15:33:42 -0300

    feat: add kafak notification controller

--- src/infra/messaging/kafka/controllers/notifications.controllers.ts
+++ src/infra/messaging/kafka/controllers/notifications.controllers.ts
@@ -1,10 +1,25 @@
+import { SendNotification } from '@application/use-cases/send-notification';
 import { Controller } from '@nestjs/common/decorators';
-import { EventPattern } from '@nestjs/microservices';
+import { EventPattern, Payload } from '@nestjs/microservices';
+
+interface SendNotificationPayload {
+  content: string;
+  category: string;
+  recipientId: string;
+}
 
 @Controller()
 export class NotificationController {
+  constructor(private sendNotification: SendNotification) {}
+
   @EventPattern('notifications.send-notification')
-  async handleSendNotification() {
-    console.log('asd');
+  async handleSendNotification(
+    @Payload() { content, category, recipientId }: SendNotificationPayload,
+  ) {
+    await this.sendNotification.execute({
+      content,
+      category,
+      recipientId,
+    });
   }
 }
